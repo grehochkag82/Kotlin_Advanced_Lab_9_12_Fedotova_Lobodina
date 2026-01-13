6009f64568601bc21930c791927d84b57f5c17a9
rampage2134 <medvedev201808@gmail.com>
2026-01-13 14:36:19 +0300

    Fixed file error

--- src/OutpostWorker.kt
+++ src/OutpostWorker.kt
@@ -1,4 +1,6 @@
 class OutpostWorker(val name: String) {
+    var maxEnergy: Int = 100
+        private set
     var energy: Int = maxEnergy
         set(value) {
             field = value.coerceIn(0, maxEnergy)
@@ -6,8 +8,6 @@ class OutpostWorker(val name: String) {
     var mood: Int = 50
         get() = field + (energy / 10)
     var level: Int = 1
-        private set
-    var maxEnergy: Int = 100
         private set
     fun work() {
         println("$name выполняет работу...")
